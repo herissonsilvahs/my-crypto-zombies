03c9faf8c556d2cbbdb73c9706d87baf003fc57b
Herisson Silva <herissonsilvahs@gmail.com>
2022-02-26 12:15:42 -0300

    feat: transfer zombieOwnership

--- ZombieOwnership.sol
+++ ZombieOwnership.sol
@@ -4,6 +4,9 @@ import './ZombieAttack.sol';
 import 'erc721.sol';
 
 contract ZombieOwnership is ZombieBattle, ERC721 {
+
+  mapping(uint => address) zombieApprovals;
+
   function balanceOf(address _owner) external view returns (uint _balance) {
     return ownerZombieCount[_owner];
   }
@@ -12,10 +15,26 @@ contract ZombieOwnership is ZombieBattle, ERC721 {
     return zombieToOwner[_tokenId];
   }
 
-  function transfer(address _to, uint _tokenId) external {}
+  function _transfer(address _from, address _to, uint _tokenId) private {
+    ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].sub(1);
+    ownerZombieCount[_to] = ownerZombieCount[_to].add(1);
+    zombieToOwner[_tokenId] = _to;
+    Transfer(_from, _to, _tokenId);
+  }
 
-  function approve(address _to, uint _tokenId) external {}
+  function transfer(address _to, uint _tokenId) external onlyOwnerOf(_tokenId) {
+    _transfer(msg.sender, _to, _tokenId);
+  }
 
-  function takeOwnership(uint _tokenId) external {}
+  function approve(address _to, uint _tokenId) external onlyOwnerOf(_tokenId) {
+    zombieApprovals[_tokenId] = _to;
+    Approval(msg.sender, _to, _tokenId);
+  }
+
+  function takeOwnership(uint _tokenId) external {
+    require(zombieApprovals[_tokenId] == msg.sender);
+    address owner = ownerOf(_tokenId);
+    _transfer(owner, msg.sender, _tokenId);
+  }
 
 }
