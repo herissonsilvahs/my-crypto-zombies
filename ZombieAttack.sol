88d66f7ff05a9169d6c82e643a549fc50b2451e9
Herisson Silva <herissonsilvahs@gmail.com>
2022-02-26 10:15:08 -0300

    feat: zombie attack function

--- ZombieAttack.sol
+++ ZombieAttack.sol
@@ -11,5 +11,19 @@ contract ZombieBattle is ZombieHelper {
     uint random = uint(keccak256(now, msg.sender, randNonce)) % _modulus;
   }
 
-  function attack(uint _zombieId, uint _targetId) external {}
+  function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
+    Zombie storage myZombie = zombies[_zombieId];
+    Zombie storage enemyZombie = zombies[_targetId];
+    uint rand = randMod(100);
+    if (rand <= attackVictoryProbability) {
+      myZombie.winCount++;
+      myZombie.level++;
+      enemyZombie.lossCount++;
+      feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
+    } else {
+      myZombie.lossCount++;
+      enemyZombie.winCount++;
+    }
+    _triggerCooldown(myZombie);
+  }
 }
