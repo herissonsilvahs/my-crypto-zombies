07819aecbec5620653771de1362f9bff9689a73e
Herisson Silva <herissonsilvahs@gmail.com>
2022-03-20 12:49:00 -0300

    config: update truffle-config

--- crypto-zombie-backend/truffle-config.js
+++ crypto-zombie-backend/truffle-config.js
@@ -1,14 +1,15 @@
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
+      host: "127.0.0.1",
       port: 8545,
-      network_id: "*", // Match any network id
+      network_id: "*",
       gas: 5000000
     }
   },
   compilers: {
     solc: {
+      version: "0.8.12",
       settings: {
         optimizer: {
           enabled: true, // Default: false
