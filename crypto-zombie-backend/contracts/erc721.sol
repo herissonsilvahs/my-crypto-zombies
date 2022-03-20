83e1746f0aa1b40a2ad5205032912a407d501ba4
Herisson Silva <herissonsilvahs@gmail.com>
2022-03-20 14:30:57 -0300

    refactor: change solidity version

--- crypto-zombie-backend/contracts/erc721.sol
+++ crypto-zombie-backend/contracts/erc721.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.12;
 
 interface ERC721 {
   event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
