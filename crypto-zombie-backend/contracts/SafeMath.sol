2312a41e9180ba5763efcbdc52cd7e6b27e0472f
Herisson Silva <herissonsilvahs@gmail.com>
2022-03-20 16:37:24 -0300

    refactor: adaptation to solidity 0.8

--- crypto-zombie-backend/contracts/SafeMath.sol
+++ crypto-zombie-backend/contracts/SafeMath.sol
@@ -36,8 +36,8 @@ library SafeMath {
   /**
   * @dev Adds two numbers, throws on overflow.
   */
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
+  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    c = a + b;
     assert(c >= a);
     return c;
   }
